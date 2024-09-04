7151d3b198785f49037a899a3a4c709f6e669e6e
Sakib-2004043 <sakib1514817122@gmail.com>
2024-09-04 17:49:53 +0600

    5.49

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 
+const name: string = "Sakib"
+const age: number = 20
+const isMan: boolean = true
+const hobbies: string[] = ["Fishing ", "Reading ", "Writing "]
+const role:[number[], string][] = [[[1,2,3], " Sakib "],[[4,5,6], " Rakib "]]
+
+
+
 function App() {
   return (
     <div className="App">
-      Hellow World
+      Hellow World {name} {age} {isMan?"Man":"Woman"}
+      <br />
+      Hobbies : {hobbies}
+      <br />
+      Tupples : {role}
+
     </div>
   );
 }
